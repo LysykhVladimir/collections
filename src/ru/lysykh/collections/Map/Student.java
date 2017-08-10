27cad214db89e0ec82876bb12b3c667479070d88
LysykhVladimir <lysykh@live.ru>
2017-08-10 23:13:13 +0300

    ArrayBasedMap implementation

---
+++ src/ru/lysykh/collections/Map/Student.java
@@ -0,0 +1,16 @@
+package ru.lysykh.collections.Map;
+
+import java.util.*;
+import java.util.Map.*;
+
+/**
+ * Created by Владимир on 07.08.2017.
+ */
+public class Student {
+
+    private final String uuid;
+
+    public Student(final String uuid) {
+        this.uuid = uuid;
+    }
+}
